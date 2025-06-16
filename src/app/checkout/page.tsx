2230009f6809ccca146c5627f0625eb46e47a68c
hanseungyuun <166717941+hanseungyuun@users.noreply.github.com>
2025-06-16 23:11:36 +0900

    Update page.tsx

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,5 +1,6 @@
 // CheckoutPage
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Link from 'next/link';
 import { ProductItem } from "@/types/Product";
 
 interface CheckoutItem {
